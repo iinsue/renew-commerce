f7bc4b3f5515b4cee8db5f33a1b72f3e78d0bacb
insu <hinsu93@gmail.com>
2024-07-17 12:29:12 +0900

    로그인 시 보일 User Button 추가

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className="h-full text-slate-500">
-      <div>Home</div>
+    <main className="h-full bg-slate-200 p-2">
+      <UserButton afterSwitchSessionUrl="/" />
     </main>
   );
 }
