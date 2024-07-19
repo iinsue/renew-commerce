aaacf7503a5a03b2539f9fdd48458c887d44ff69
iinsue <hinsu93@naver.com>
2024-07-19 21:04:34 +0900

    #8-6 FE에 가게 이름 수정 API 연동하기

--- app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/_components/settings-form.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import * as z from "zod";
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { Store } from "@prisma/client";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Trash } from "lucide-react";
+import { Loader2, Trash } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -19,6 +19,9 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Heading } from "@/components/ui/heading";
+import { useParams, useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "sonner";
 
 type Props = {
   initialData: Store;
@@ -29,8 +32,11 @@ const formSchema = z.object({
 });
 
 export const SettingsForm: React.FC<Props> = ({ initialData }) => {
+  const router = useRouter();
+  const params = useParams();
+
   const [isOpen, setIsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isPending, setStartTransition] = useTransition();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -38,7 +44,15 @@ export const SettingsForm: React.FC<Props> = ({ initialData }) => {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    setStartTransition(async () => {
+      try {
+        await axios.patch(`/api/stores/${params.storeId}`, values);
+        router.refresh();
+        toast.success("가게 이름이 변경되었습니다.", { id: "update-success" });
+      } catch (error) {
+        toast.error("이름 변경에 실패했습니다.");
+      }
+    });
   };
 
   return (
@@ -46,7 +60,7 @@ export const SettingsForm: React.FC<Props> = ({ initialData }) => {
       <div className="flex items-center justify-between">
         <Heading title="Settings" description="Manage store preferences" />
         <Button
-          disabled={isLoading}
+          disabled={isPending}
           variant="destructive"
           size="icon"
           onClick={() => setIsOpen(true)}
@@ -69,7 +83,7 @@ export const SettingsForm: React.FC<Props> = ({ initialData }) => {
                   <FormLabel>Name</FormLabel>
                   <FormControl>
                     <Input
-                      disabled={isLoading}
+                      disabled={isPending}
                       placeholder="Store name"
                       {...field}
                     />
@@ -79,7 +93,8 @@ export const SettingsForm: React.FC<Props> = ({ initialData }) => {
               )}
             />
           </div>
-          <Button disabled={isLoading} className="ml-auto" type="submit">
+          <Button disabled={isPending} className="ml-auto" type="submit">
+            {isPending && <Loader2 className="mr-2 size-4 animate-spin" />}
             Save changes
           </Button>
         </form>
