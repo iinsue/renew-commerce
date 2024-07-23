163802fbd797bc71f1f801aacd0a818ec911fb7a
insu <hinsu93@naver.com>
2024-07-23 18:53:55 +0900

    #18-4. 사이즈 리스트 조회

--- app/(dashboard)/[storeId]/(routes)/sizes/_components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/_components/client.tsx
@@ -1,7 +1,41 @@
-export const SizeClient = () => {
+"use client";
+
+import { useParams, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
+import { columns, SizeColumn } from "./columns";
+
+import { Plus } from "lucide-react";
+
+type Props = {
+  data: SizeColumn[];
+};
+
+export const SizeClient: React.FC<Props> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+
   return (
     <>
-      <div>Size Client Component</div>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Sizes (${data.length})`}
+          description="사이즈를 관리할 수 있습니다."
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/sizes/new`)}>
+          <Plus className="mr-2 size-4" />
+          사이즈 생성
+        </Button>
+      </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description="사이즈를 위한 API를 제공합니다." />
+      <Separator />
+      <ApiList entityName="sizes" entityIdName="sizeId" />
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -1,10 +1,33 @@
+import db from "@/lib/prismadb";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
 import { SizeClient } from "./_components/client";
+import { SizeColumn } from "./_components/columns";
 
-const SizesPage = () => {
+const SizesPage = async ({ params }: { params: { storeId: string } }) => {
+  // DB에서 사이즈 조회
+  const sizes = await db.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  // 조회한 사이즈 데이터 가공
+  const formattedSizes: SizeColumn[] = sizes.map((size) => ({
+    id: size.id,
+    name: size.name,
+    value: size.value,
+    createdAt: format(size.createdAt, "yyyy-MM-dd", { locale: ko }),
+  }));
+
   return (
     <>
-      <div>Sizes Page</div>
-      <SizeClient />
+      <div className="space-y-4 p-8 pt-6">
+        <SizeClient data={formattedSizes} />
+      </div>
     </>
   );
 };
