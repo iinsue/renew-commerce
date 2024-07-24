6acf9e4596532c874239a4f066aa91df111819a4
iinsue <hinsu93@naver.com>
2024-07-24 22:29:58 +0900

    #21-1. 아이디 복사 버튼 추가

--- app/(dashboard)/[storeId]/(routes)/sizes/_components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/_components/cell-action.tsx
@@ -10,8 +10,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
-import { Edit, MoreHorizontal } from "lucide-react";
+import { Copy, Edit, MoreHorizontal } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
+import { toast } from "sonner";
 
 type Props = {
   data: SizeColumn;
@@ -21,6 +22,15 @@ export const CellAction: React.FC<Props> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
+  // 클립보드에 사이즈 아이디 복사
+  const onCopyClick = () => {
+    navigator.clipboard.writeText(data.id);
+    toast.success("클립보드에 복사되었습니다.", {
+      id: "copy-to-clipboard",
+    });
+  };
+
+  // 수정페이지로 이동
   const onUpdateClick = () => {
     router.push(`/${params.storeId}/sizes/${data.id}`);
   };
@@ -36,6 +46,10 @@ export const CellAction: React.FC<Props> = ({ data }) => {
         <DropdownMenuContent align="end">
           <DropdownMenuLabel>Actions</DropdownMenuLabel>
           <Separator />
+          <DropdownMenuItem onClick={onCopyClick}>
+            <Copy className="mr-2 size-4" />
+            아이디 복사
+          </DropdownMenuItem>
           <DropdownMenuItem onClick={onUpdateClick}>
             <Edit className="mr-2 size-4" />
             사이즈 수정
