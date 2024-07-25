ee11bf59f7a2cbaa0deccae27aa606d15b42e894
insu <hinsu93@gmail.com>
2024-07-25 12:55:50 +0900

    #22-3. 색상 리스트 조회 기능 추가

--- app/(dashboard)/[storeId]/(routes)/colors/_components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/_components/client.tsx
@@ -1,11 +1,21 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Heading } from "@/components/ui/heading";
-import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
-export const ColorsClient = () => {
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { ApiList } from "@/components/ui/api-list";
+import { DataTable } from "@/components/ui/data-table";
+
+import { ColorColumn, columns } from "./columns";
+import { Plus } from "lucide-react";
+
+type Props = {
+  data: ColorColumn[];
+};
+
+export const ColorsClient: React.FC<Props> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
@@ -16,7 +26,7 @@ export const ColorsClient = () => {
 
   return (
     <>
-      <div>
+      <div className="flex items-center justify-between">
         <Heading
           title={`Colors (0)`}
           description="색상을 관리할 수 있습니다."
@@ -26,6 +36,11 @@ export const ColorsClient = () => {
           색상 추가
         </Button>
       </div>
+      <Separator />
+      <DataTable searchKey="name" columns={columns} data={data} />
+      <Heading title="API" description=" 색상을 위한 API를 제공합니다." />
+      <Separator />
+      <ApiList entityName="colors" entityIdName="colorId" />
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/colors/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -1,10 +1,32 @@
-import { ColorsClient } from "./_components/client";
+import db from "@/lib/prismadb";
+import { format } from "date-fns";
 
-const ColorsPage = () => {
+import { ColorsClient } from "./_components/client";
+import { ko } from "date-fns/locale";
+
+const ColorsPage = async ({ params }: { params: { storeId: string } }) => {
+  // DB에서 색상 리스트 조회
+  const colors = await db.color.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  // 색상 리스트 데이터 가공
+  const formattedColors = colors.map((color) => ({
+    id: color.id,
+    name: color.name,
+    value: color.value,
+    createdAt: format(color.createdAt, "yyyy-MM-dd", { locale: ko }),
+  }));
+
   return (
     <>
       <div className="space-y-4 p-8 pt-6">
-        <ColorsClient />
+        <ColorsClient data={formattedColors} />
       </div>
     </>
   );
