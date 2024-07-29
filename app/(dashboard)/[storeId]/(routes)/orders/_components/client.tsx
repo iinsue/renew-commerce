15d53bd70c4cedbfde5bd55efe390a17637d4027
insu <hinsu93@gmail.com>
2024-07-29 18:56:11 +0900

    #30-2. 주문 리스트 조회 페이지 추가

---
+++ app/(dashboard)/[storeId]/(routes)/orders/_components/client.tsx
@@ -0,0 +1,15 @@
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
+export const OrdersClient = () => {
+  return (
+    <>
+      <Heading
+        title="주문(0)"
+        description="주문정보조회를 위한 API를 제공합니다."
+      />
+      <Separator />
+      {/* TODO: 주문 리스트 */}
+    </>
+  );
+};
---
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -0,0 +1,13 @@
+import { OrdersClient } from "./_components/client";
+
+const OrdersPage = () => {
+  return (
+    <>
+      <div className="space-y-4 p-8 pt-6">
+        <OrdersClient />
+      </div>
+    </>
+  );
+};
+
+export default OrdersPage;
