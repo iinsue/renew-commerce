47a297a9307d5db309dfbe050c0a3a78f7ebeb95
insu <hinsu93@gmail.com>
2024-07-29 19:24:46 +0900

    #30.5 Products 리스트 조회 오류 수정

--- app/(dashboard)/[storeId]/(routes)/products/_components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/_components/client.tsx
@@ -26,7 +26,10 @@ export const ProductClient: React.FC<Props> = ({ data }) => {
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading title={`상품 (0)`} description="상품을 관리할 수 있습니다." />
+        <Heading
+          title={`상품 (${data.length})`}
+          description="상품을 관리할 수 있습니다."
+        />
         <Button onClick={onAddClick}>
           <Plus className="mr-2 size-4" />
           상품 추가
