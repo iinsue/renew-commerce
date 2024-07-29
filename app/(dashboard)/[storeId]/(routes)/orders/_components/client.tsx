2ee8f7608e77a5dc5470614c7ad249af11353f6a
insu <hinsu93@gmail.com>
2024-07-29 19:21:05 +0900

    #30-3. 주문 리스트 조회 기능 추가

--- app/(dashboard)/[storeId]/(routes)/orders/_components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/_components/client.tsx
@@ -1,15 +1,21 @@
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { columns, OrderColumn } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
-export const OrdersClient = () => {
+type Props = {
+  data: OrderColumn[];
+};
+
+export const OrdersClient: React.FC<Props> = ({ data }) => {
   return (
     <>
       <Heading
-        title="주문(0)"
+        title={`주문 (${data.length})`}
         description="주문정보조회를 위한 API를 제공합니다."
       />
       <Separator />
-      {/* TODO: 주문 리스트 */}
+      <DataTable searchKey="label" columns={columns} data={data} />
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -1,10 +1,43 @@
+import db from "@/lib/prismadb";
 import { OrdersClient } from "./_components/client";
+import { formatter } from "@/lib/utils";
 
-const OrdersPage = () => {
+const OrdersPage = async ({ params }: { params: { storeId: string } }) => {
+  const orders = await db.order.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      orderItems: {
+        include: {
+          product: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedOrders = orders.map((order) => ({
+    id: order.id,
+    phone: order.phone,
+    address: order.address,
+    products: order.orderItems
+      .map((orderItem) => orderItem.product.name)
+      .join(", "),
+    totalPrice: formatter.format(
+      order.orderItems.reduce((total, item) => {
+        return total + Number(item.product.price);
+      }, 0),
+    ),
+    isPaid: order.isPaid,
+  }));
+
   return (
     <>
       <div className="space-y-4 p-8 pt-6">
-        <OrdersClient />
+        <OrdersClient data={formattedOrders} />
       </div>
     </>
   );
