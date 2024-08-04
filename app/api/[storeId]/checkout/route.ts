df3ce9dc48345e7881d3eea13f53da8649f3aa54
iinsue <hinsu93@naver.com>
2024-08-04 22:34:11 +0900

    Fix : 금액 * 100 제거

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -42,7 +42,7 @@ export async function POST(
         product_data: {
           name: product.name,
         },
-        unit_amount: Number(product.price) * 100,
+        unit_amount: Number(product.price),
       },
     });
   });
