e3458a9bdae468c8434bee97922287d523bf4629
iinsue <hinsu93@naver.com>
2024-08-05 21:50:26 +0900

    #32. Dashboard - Card

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,21 +1,63 @@
 import db from "@/lib/prismadb";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { formatter } from "@/lib/utils";
+import { CreditCard, DollarSign, Package } from "lucide-react";
 
 type Props = {
   params: { storeId: string };
 };
 
 const DashboardPage: React.FC<Props> = async ({ params }: Props) => {
-  // DB 상에서 해당하는 아이디의 가게 정보를 가져옵니다.
-  const store = await db.store.findFirst({
-    where: {
-      id: params.storeId,
-    },
-  });
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
 
   return (
-    <>
-      <div>활성화된 스토어: {store?.name}</div>
-    </>
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="대시보드" description="매장 개요" />
+        <Separator />
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">총 수입</CardTitle>
+              <DollarSign className="size-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">판매수</CardTitle>
+              <CreditCard className="size-5 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">+{salesCount}</div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                재고가 있는 제품
+              </CardTitle>
+              <CreditCard className="size-5 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">{stockCount}</div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
   );
 };
 
